753601379073c050155565f424db51c5bd6e8328
ispodarik <ispodarik@diasoft.ru>
2020-01-12 14:53:53 +0300

    Unused import.

--- hw05/src/test/java/ru/otus/hw/db/BookImplTest.java
+++ hw05/src/test/java/ru/otus/hw/db/BookImplTest.java
@@ -3,7 +3,6 @@ package ru.otus.hw.db;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.otus.hw.db.book.BookDBProvider;
 import ru.otus.hw.entity.Author;
